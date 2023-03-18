81d68997c42effc478370206cb7fb78b92a858ee
CondorW <rotsteiner@gmail.com>
2023-03-18 19:11:51 +0100

    Rework Contact Section, implement emails for links.

--- src/components/ContactSection.svelte
+++ src/components/ContactSection.svelte
@@ -7,87 +7,20 @@
 				corporis quia asperiores dolorem nisi corrupti eveniet dolores ad maiores repellendus enim
 				autem omnis fugiat perspiciatis? Ad, veritatis.
 			</p>
-			<p class="text-base-content mb-2">New York, 94126, United States</p>
-			<p class="text-base-content mb-2">+ 01 234 567 89</p>
-			<p class="text-base-content mb-2">info@gmail.com</p>
+			<p class="text-base-content mb-2">Grünentorgasse 12/5, 1010 Wien</p>
+			
+			<p class="text-base-content mb-2">dataestateaustria@gmail.com</p>
 		</div>
-		<div class="grow-0 shrink-0 basis-auto mb-12 md:mb-0 w-full md:w-6/12 px-3 lg:px-6">
-			<form>
-				<div class="form-group mb-6">
-					<input
-						type="text"
-						class="form-control block
-              w-full
-              px-3
-              py-1.5
-              text-base
-              font-normal
-              text-gray-700
-              bg-white bg-clip-padding
-              border border-solid border-gray-300
-              rounded
-              transition
-              ease-in-out
-              m-0
-              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-						id="exampleInput7"
-						placeholder="Name"
-					/>
-				</div>
-				<div class="form-group mb-6">
-					<input
-						type="email"
-						class="form-control block
-              w-full
-              px-3
-              py-1.5
-              text-base
-              font-normal
-              text-gray-700
-              bg-white bg-clip-padding
-              border border-solid border-gray-300
-              rounded
-              transition
-              ease-in-out
-              m-0
-              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-						id="exampleInput8"
-						placeholder="Email address"
-					/>
-				</div>
-				<div class="form-group mb-6">
-					<textarea
+		<div class="grow-0 shrink-0 basis-auto mb-12 md:mb-0 w-full md:w-6/12 px-3 lg:px-6">	
+				<div class="form-group form-check text-center mb-6" />
+				<a href="mailto:dataestateaustria@gmail.com">
+					<button
+						type="submit"
 						class="
-              form-control
-              block
-              w-full
-              px-3
-              py-1.5
-              text-base
-              font-normal
-              text-gray-700
-              bg-white bg-clip-padding
-              border border-solid border-gray-300
-              rounded
-              transition
-              ease-in-out
-              m-0
-              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-            "
-						id="exampleFormControlTextarea13"
-						rows="3"
-						placeholder="Message"
-					/>
-				</div>
-				<div class="form-group form-check text-center mb-6">
-				</div>
-				<button
-					type="submit"
-					class="
             w-full
             px-6
             py-2.5
-            bg-base-content
+            bg-primary
             text-white
             font-medium
             text-xs
@@ -101,8 +34,8 @@
             transition
             duration-150
             ease-in-out">Send</button
-				>
-			</form>
+					>
+				</a>
 		</div>
 	</div>
 </section>
