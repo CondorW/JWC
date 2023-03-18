5f78af8782931f0d304841bfa4f39612df09c85d
CondorW <rotsteiner@gmail.com>
2023-03-18 19:14:42 +0100

    Redesign layout so that footer stays on bottom even in a page with a lot of content. Add pages privacy policy and imprint.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,57 @@
-<script>
-    import "../app.css";
-	import Card from "../components/Card.svelte";
-	import Footer from "../components/Footer.svelte";
-  </script>
-  
-<html lang="english" data-theme="halloween"></html>
 
-<body>
-  
-  <!-- Navbar -->
-  <div class="navbar bg-base-300">
-    <div class="flex-1">
-      <a href="/" class="btn btn-ghost normal-case text-xl">Data Estate Austria</a>
-    </div>
-    <div class="flex-none">
-      <button class="btn btn-square btn-ghost">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-      </button>
-    </div>
-  </div>
-  <!-- Hero Section Content -->
-  <slot></slot>
-  
+<script lang="ts">
+	import '../app.css';
+	import Footer from '../components/Footer.svelte';
+
+
+	// try {
+	// 	if (typeof window !== 'undefined' && window) {
+	// 		const googleAnalyticsId = 'YOUR_MEASUREMENT_ID_HERE';
+	// 		window.dataLayer = window.dataLayer || [];
+	// 		function gtag(a: string, b: object | string) {
+	// 			window.dataLayer.push();
+	// 		}
+	// 		gtag('js', new Date());
+	// 		gtag('config', googleAnalyticsId);
+	// 	}
+	// } catch (error) {
+	// 	console.error('Failed to initialize Analytics');
+	// }
+</script>
+
+<html lang="english" data-theme="halloween" />
+<!-- Sets the theme -->
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-SVSEQZT4KV"></script>
+</svelte:head>
+
+<body class="flex flex-col min-h-screen">
+	<!-- Navbar -->
+	<div class="navbar bg-base-300">
+		<div class="flex-1">
+			<a href="/" class="btn btn-ghost normal-case text-xl">Data Estate Austria</a>
+		</div>
+		<div class="flex-none">
+			<button class="btn btn-square btn-ghost">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="inline-block w-5 h-5 stroke-current"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+					/></svg
+				>
+			</button>
+		</div>
+	</div>
+	<!-- Hero Section Content -->
+	<slot />
 </body>
-<Footer></Footer>
-
+<footer class="mt-auto">
+<Footer />
+</footer>
