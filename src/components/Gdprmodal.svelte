e3b9c189a23fc06a15a93d5293afa7ae8b2b1403
CondorW <rotsteiner@gmail.com>
2023-03-26 22:18:11 +0200

    Adapt the modal display so that it does not show when user clicks on the privacy policy to read through it.

--- src/components/Gdprmodal.svelte
+++ src/components/Gdprmodal.svelte
@@ -25,7 +25,32 @@
 	}
 </script>
 
-{#if $preferences.acceptAllCookies == false}
+{#if $preferences.acceptAllCookies == false && $page.url.pathname == "/PrivacyPolicy"}
+<input type="checkbox" id="my-modal-6" class="modal-toggle" />
+<div class="modal modal-bottom sm:modal-middle">
+	<div class="modal-box">
+		<h3 class="font-bold text-lg">Cookie and GDPR Consent</h3>
+		<p class="py-4">
+			I hereby give my <strong>explicit</strong> consent regarding the processing of my personal Data.
+			<br>
+			I hereby give my <strong>explicit</strong> consent regarding the use of Cookies.
+			<br>
+			I hereby <strong>explicitly</strong> agree to the <a class="underline" href="/PrivacyPolicy">privacy policy</a>.
+			<br>
+		</p>
+		<div class="flex justify-between">
+		<button on:click={acceptGdpr} class="btn modal-action">
+			Yes I give my explicit consent
+		</button>
+		<button class="btn modal-action">
+			<a href="https://www.google.com">
+				No I deny
+			</a>
+		</button>
+	</div>
+	</div>
+</div>
+{:else if $preferences.acceptAllCookies == false}
 	<input type="checkbox" id="my-modal-6" class="modal-toggle" />
 	<div class="modal modal-bottom sm:modal-middle modal-open">
 		<div class="modal-box">
@@ -35,7 +60,7 @@
 				<br>
 				I hereby give my <strong>explicit</strong> consent regarding the use of Cookies.
 				<br>
-				I hereby <strong>explicitly</strong> agree to the privacy policy.
+				I hereby <strong>explicitly</strong> agree to the <a class="underline" href="/PrivacyPolicy">privacy policy</a>.
 				<br>
 			</p>
 			<div class="flex justify-between">
